eeaa84f733ca41e8cec6653bfaad1afe092ee233
Izabela Łach <ilach@opi.org.pl>
2019-12-04 14:13:39 +0100

    'first_comit'

---
+++ project/src/main/java/dev/proj/project/application/controller/Controller.java
@@ -0,0 +1,13 @@
+package dev.proj.project.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello world";
+    }
+}
