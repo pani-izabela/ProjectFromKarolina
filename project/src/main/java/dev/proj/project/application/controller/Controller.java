a9b2a0671eb2f3f465e6d9a7abd20729d66d98cf
Izabela Łach <ilach@opi.org.pl>
2019-12-10 09:14:00 +0100

    'metoda_addUser_dodana'

--- project/src/main/java/dev/proj/project/application/controller/Controller.java
+++ project/src/main/java/dev/proj/project/application/controller/Controller.java
@@ -3,13 +3,13 @@ package dev.proj.project.application.controller;
 import dev.proj.project.application.dao.Users;
 import dev.proj.project.application.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
 
 @RestController
 public class Controller {
@@ -30,5 +30,10 @@ public class Controller {
     @RequestMapping(value = "/getUser", method = RequestMethod.GET)
     public Optional<Users> getUser(@RequestParam Integer userId){
         return usersService.getUser(userId);
+    }
+
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST, consumes = APPLICATION_JSON_VALUE)
+    public void addUser(@RequestBody Users user){
+        usersService.saveUser(user);
     }
 }
